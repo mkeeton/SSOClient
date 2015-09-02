5184bf09672aac0174aa7f0e2c0f9e09ded3cc42
Martin Keeton <martin.keeton79@gmail.com>
2015-09-02 09:14:40 +0100

    Updated to reflect changes in BasicMVC

--- SSOTest/Controllers/HomeController.cs
+++ SSOTest/Controllers/HomeController.cs
@@ -7,14 +7,14 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 
-using Authentication.BasicMVC.Client.Models;
+using Authentication.BasicMVC.Client.Domain.Models;
 using Authentication.BasicMVC.Client.Repositories;
 using Authentication.BasicMVC.Client.Attributes;
 using Authentication.BasicMVC.Client;
 
 namespace SSOTest.Controllers
 {
-  public class HomeController : BaseController
+  public class HomeController : Controller
   {
     public ActionResult Index()
     {
@@ -29,31 +29,6 @@ namespace SSOTest.Controllers
         {
           ViewBag.TestReturn = "You are logged in as user: " + userId.ToString();
         }
-        //using (var client = new HttpClient())
-        //{
-        //    client.BaseAddress = new Uri("https://localhost:44300/");
-        //    client.DefaultRequestHeaders.Accept.Clear();
-        //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-        //    // New code:
-        //    HttpResponseMessage response = client.GetAsync("API/Authentication/" + CookieRepository.GetCookieValue("SessionID",Guid.NewGuid().ToString())).Result;
-        //    if (response.IsSuccessStatusCode)
-        //    {
-        //      AuthenticationResponse _Response = response.Content.ReadAsAsync<AuthenticationResponse>().Result;
-        //      if (_Response.ResponseCode == AuthenticationResponse.AuthenticationResponseCode.Unknown)
-        //      {
-        //        return Redirect(_Response.RedirectURL + "?sessionID=" + CookieRepository.GetCookieValue("SessionID", Guid.NewGuid().ToString()) + "&returnURL=" + HttpUtility.UrlEncode(Request.Url.Scheme + "://" + Request.Url.Authority + Request.Url.PathAndQuery));
-        //      }
-        //      else if (_Response.ResponseCode == AuthenticationResponse.AuthenticationResponseCode.NotLoggedIn)
-        //      {
-        //        ViewBag.TestReturn = "you are not logged in";
-        //      }
-        //      else if (_Response.ResponseCode == AuthenticationResponse.AuthenticationResponseCode.LoggedIn)
-        //      {
-        //        ViewBag.TestReturn = "you are logged in";
-        //      }
-        //    }
-        //}
       }
       catch(Exception ex)
       {
@@ -65,32 +40,6 @@ namespace SSOTest.Controllers
     [SSOAuthentication]
     public ActionResult Secure()
     {
-      //using (var client = new HttpClient())
-      //{
-      //  client.BaseAddress = new Uri("https://localhost:44300/");
-      //  client.DefaultRequestHeaders.Accept.Clear();
-      //  client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-      //  // New code:
-      //  HttpResponseMessage response = client.GetAsync("API/Authentication/" + Repositories.CookieRepository.GetCookieValue("SessionID",Guid.NewGuid().ToString())).Result;
-      //  if (response.IsSuccessStatusCode)
-      //  {
-      //    Authentication.BasicMVC.Domain.Models.AuthenticationResponse _Response = response.Content.ReadAsAsync<Authentication.BasicMVC.Domain.Models.AuthenticationResponse>().Result;
-      //    ViewBag.TestReturn = _Response.RedirectURL;
-      //    if (_Response.ResponseCode == Authentication.BasicMVC.Domain.Models.AuthenticationResponse.AuthenticationResponseCode.Unknown)
-      //    {
-      //      return Redirect(_Response.RedirectURL + "?sessionID=" + Repositories.CookieRepository.GetCookieValue("SessionID", Guid.NewGuid().ToString()) + "&returnURL=" + HttpUtility.UrlEncode(Request.Url.Scheme + "://" + Request.Url.Authority + Request.Url.PathAndQuery));
-      //    }
-      //    else if(_Response.ResponseCode == Authentication.BasicMVC.Domain.Models.AuthenticationResponse.AuthenticationResponseCode.NotLoggedIn)
-      //    {
-      //      return Redirect(_Response.RedirectURL + "?sessionID=" + Repositories.CookieRepository.GetCookieValue("SessionID", Guid.NewGuid().ToString()) + "&returnURL=" + HttpUtility.UrlEncode(Request.Url.Scheme + "://" + Request.Url.Authority + Request.Url.PathAndQuery));
-      //    }
-      //    else if (_Response.ResponseCode == Authentication.BasicMVC.Domain.Models.AuthenticationResponse.AuthenticationResponseCode.LoggedIn)
-      //    {
-      //      ViewBag.TestReturn = "you are logged in";
-      //    }
-      //  }
-      //}
       return View();
     }
 
